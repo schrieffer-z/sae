352456384cfcb3e774129f3ec85073fb2b817f9a
schrieffer-z <schrieffer.zsy@gmail.com>
2025-05-06 16:45:08 +0000

    init

--- recipes/sae_std_train.sh
+++ recipes/sae_std_train.sh
@@ -41,7 +41,7 @@ python -u main.py --model_path $model_path --hidden_size $hidden_size \
     --sequence_or_token token \
     --dataset_name $dataset_name \
     --pipe_data_path $train_data_path $eval_data_path $apply_data_path --layer $layer --latent_size $latent_size \
-    --batch_size $batch_size --max_length $max_length --lr 5e-4 --betas 0.9 0.999 --num_epochs 1 --seed 42 --steps 10 --use_wandb 1 \
+    --batch_size $batch_size --max_length $max_length --lr 5e-4 --betas 0.9 0.999 --num_epochs 1 --seed 42 --steps 10 --use_wandb $use_wandb \
     --pipe_project $train_project $eval_project $pipe_project --device $device --k $k \
     --api_base $api_base --api_key $api_key --api_version $api_version --engine $engine
 
