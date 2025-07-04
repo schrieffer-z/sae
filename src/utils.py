31f3a485fa40f7505c6ddb1c25a02f44e2f615d8
schrieffer-z <schrieffer.zsy@gmail.com>
2025-07-04 17:23:23 +0800

    init

--- src/utils.py
+++ src/utils.py
@@ -840,7 +840,7 @@ class SequenceApplier:
             save_ats = np.round(len(self.dataloader)*np.linspace(0,1,11))[1:-1].astype(np.int64)
             if (global_step_idx in save_ats):
                 output_path_tmp = f'../contexts/tmp/{os.path.splitext(os.path.basename(self.cfg.SAE_path))[0]}_{threshold}@step{global_step_idx}.json'
-                save_latent_dict(latent_context_map, output_path_tmp, threshold, max_length, max_per_token)
+                save_latent_dict(latent_context_map, output_path_tmp, threshold, max_length, max_per_token, lines)
             global_step_idx += 1
 
 
